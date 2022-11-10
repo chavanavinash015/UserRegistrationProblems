0baf0d1f2fa29be9d2193618f08712795936e17c
arc-015 <chavanavinash015@gmail.com>
2022-11-10 20:12:54 +0530

    User Registration test password1

--- src/main/java/com/bridgelabz/UserRegistration/UserValidation.java
+++ src/main/java/com/bridgelabz/UserRegistration/UserValidation.java
@@ -7,6 +7,7 @@ public class UserValidation {
     public static final String EMAIL_ID = "^[A-Z]{1,}[a-z]{5,}[@]{1}[a-z]{2,}[.]?[a-z]{2,}$";
     public static final String PHONE_NUMBER = "^[0-9]{2}+\s[0-9]{10}$";
 
+    public static final String PASSWORD1 = "^[a-z]{8,}$";
     public Boolean checkFirstName(String firstName) {
         return (Pattern.matches(VALIDATE_NAME, firstName));
     }
@@ -22,4 +23,9 @@ public class UserValidation {
     public Boolean checkPhoneNumber(String phone_Number) {
         return (Pattern.matches(PHONE_NUMBER,phone_Number));
     }
+
+    public Boolean checkPassword(String password1) {
+        return (Pattern.matches(PASSWORD1,password1));
+    }
+
 }
